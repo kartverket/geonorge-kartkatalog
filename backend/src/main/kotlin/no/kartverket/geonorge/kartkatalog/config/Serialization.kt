18b79c50243b307fe799f391d603d4aff993c45f
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2026-06-19 09:03:48 +0200

    Formatterer importer

--- backend/src/main/kotlin/no/kartverket/geonorge/kartkatalog/config/Serialization.kt
+++ backend/src/main/kotlin/no/kartverket/geonorge/kartkatalog/config/Serialization.kt
@@ -1,8 +1,8 @@
 package no.kartverket.geonorge.kartkatalog.config
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 fun Application.configureSerialization() {
