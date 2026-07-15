f317e2ee53e8846943180520c9abedfb3e6ea9ea
annaaurora <anna-aurora@live.no>
2026-07-15 17:27:34 +0200

    trekk søkefeltet ut i searchField

--- frontend/src/components/Header/HeaderSearch.tsx
+++ frontend/src/components/Header/HeaderSearch.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import { Search } from "@kv-designsystem/react";
 import styles from "./HeaderSearch.module.css";
+import { SearchField } from "./SearchField";
 
 export function HeaderSearch() {
   return (
     <search className={styles.panel}>
-      <Search data-color="neutral">
-        <Search.Input
-          aria-label="Søk i kartkatalogen"
-          placeholder="FKB-bygning, Rødlistede arter..."
-        />
-        <Search.Clear />
-      </Search>
+      <SearchField />
     </search>
   );
 }
---
+++ frontend/src/components/Header/SearchField.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Divider, Search } from "@kv-designsystem/react";
+
+export function SearchField() {
+  return (
+    <Search data-color="neutral">
+      <Search.Input
+        aria-label="Søk i kartkatalogen"
+        placeholder="FKB-bygning, Rødlistede arter..."
+      />
+      <Search.Clear />
+    </Search>
+  );
+}
