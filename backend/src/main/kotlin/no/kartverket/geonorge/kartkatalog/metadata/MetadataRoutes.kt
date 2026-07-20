2984589d89e3518d6ce7c69894fa63d8d3646edc
ingebjrgb <ingebjrgb@gmail.com>
2026-07-20 08:59:59 +0200

    bygger opp metadatarouting samlet

--- backend/src/main/kotlin/no/kartverket/geonorge/kartkatalog/metadata/MetadataRoutes.kt
+++ backend/src/main/kotlin/no/kartverket/geonorge/kartkatalog/metadata/MetadataRoutes.kt
@@ -4,26 +4,30 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 import java.util.UUID
 
 fun Route.metadataRoutes(metadataSummaryService: MetadataSummaryService) {
-    get("/metadata/{uuid}") {
-        val uuid = UUID.fromString(call.parameters["uuid"])
+    // TODO: se på grupperingen av data og navngivning
+    route("/metadata/") {
+        get("summary/{uuid}") {
+            val uuid = UUID.fromString(call.parameters["uuid"])
 
-        val result = metadataSummaryService.getMetadataSummary(uuid)
-        if (result == null) {
-            call.respond(HttpStatusCode.NotFound, mapOf("error" to "Record not found"))
-            return@get
+            val result = metadataSummaryService.getMetadataSummary(uuid)
+            if (result == null) {
+                call.respond(HttpStatusCode.NotFound, mapOf("error" to "Record not found"))
+                return@get
+            }
+
+            call.respond(result)
         }
 
-        call.respond(result)
-    }
+        get("/info/{uuid}") {
+            val uuid = UUID.fromString(call.parameters["uuid"])
 
-    get("/metadatainfo/{uuid}") {
-        val uuid = UUID.fromString(call.parameters["uuid"])
+            val result = metadataSummaryService.getMetadataInformation(uuid)
 
-        val result = metadataSummaryService.getMetadataInformation(uuid)
-
-        call.respond(result)
+            call.respond(result)
+        }
     }
 }
