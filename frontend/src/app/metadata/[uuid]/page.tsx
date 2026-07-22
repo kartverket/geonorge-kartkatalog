79055ca3556318b92d4306afc7b28f0826f80d6e
ingebjrgb <ingebjrgb@gmail.com>
2026-07-22 13:26:35 +0200

    bruker data fra getmetadatasummary til å fylle datasetheader, samt legger til en simpel loading screen

--- frontend/src/app/metadata/[uuid]/page.tsx
+++ frontend/src/app/metadata/[uuid]/page.tsx
@@ -1,3 +1,4 @@
+import { getMetadataSummary } from "@/app/api";
 import getData from "../../../mocks/getData.json";
 import { DatasetActions } from "./_components/DatasetActions";
 import { DatasetHeader } from "./_components/DatasetHeader";
@@ -5,16 +6,24 @@ import { DatasetMeta } from "./_components/DatasetMeta";
 import { DatasetThumbnail } from "./_components/DatasetThumbnail";
 import styles from "./page.module.css";
 
-export default async function DatasetPage() {
+export default async function DatasetPage({
+  params,
+}: {
+  params: Promise<{ uuid: string }>;
+}) {
   const d = getData;
   const thumb =
     d.Thumbnails.find((t) => t.Type === "medium")?.URL ?? d.Thumbnails[0]?.URL;
+
+  const { uuid } = await params;
+  const metadataSummary = await getMetadataSummary(uuid);
+
   return (
     <div className={styles.content}>
       <DatasetHeader
-        title={d.Title}
-        organization={d.Organization}
-        isOpen={d.AccessIsOpendata}
+        title={metadataSummary.title ?? "tittel"}
+        organization={metadataSummary.organization ?? "org"}
+        isOpen={metadataSummary.accessIsOpenData ?? true}
       />
       <div className={styles.metaRow}>
         <DatasetThumbnail thumbnailUrl={thumb} />
