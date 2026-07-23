5ea27c1a9366b750006b11bf8a10ea3e5595d214
ingebjrgb <ingebjrgb@gmail.com>
2026-07-23 13:03:21 +0200

    legger inn dummy-errorfil

---
+++ frontend/src/app/metadata/[uuid]/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect } from "react";
+
+export default function ErrorPage({
+  error,
+  unstable_retry,
+}: {
+  error: Error & { digest?: string };
+  unstable_retry: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return <div>Feil</div>;
+}
