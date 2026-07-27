589db91e2e8f229b40d923584bcd3964607d51c2
annaaurora <anna-aurora@live.no>
2026-07-27 17:33:31 +0200

    ktlint

--- backend/src/main/kotlin/no/kartverket/geonorge/kartkatalog/metadata/MetadataService.kt
+++ backend/src/main/kotlin/no/kartverket/geonorge/kartkatalog/metadata/MetadataService.kt
@@ -8,8 +8,8 @@ import no.kartverket.geonorge.kartkatalog.integrations.geonetwork.model.Metadata
 import no.kartverket.geonorge.kartkatalog.integrations.register.CodeList
 import no.kartverket.geonorge.kartkatalog.integrations.register.RegisterClient
 import no.kartverket.geonorge.kartkatalog.metadata.models.ProductDataQualityMeasure
-import no.kartverket.geonorge.kartkatalog.metadata.models.ProductFairStatus
 import no.kartverket.geonorge.kartkatalog.metadata.models.ProductDistributionFormat
+import no.kartverket.geonorge.kartkatalog.metadata.models.ProductFairStatus
 import no.kartverket.geonorge.kartkatalog.metadata.models.ProductKeyword
 import no.kartverket.geonorge.kartkatalog.metadata.models.ProductMetadataContact
 import no.kartverket.geonorge.kartkatalog.metadata.models.ProductMetadataInfo
