2653ff1cb0fa5bb25e2c83ed684e2c795574605c
ingebjrgb <ingebjrgb@gmail.com>
2026-07-31 12:48:37 +0200

    legger til lenke for metadataxml

--- frontend/src/app/metadata/[uuid]/_components/ProductActions.tsx
+++ frontend/src/app/metadata/[uuid]/_components/ProductActions.tsx
@@ -4,22 +4,20 @@ import {
   FileTextIcon,
   PencilIcon,
 } from "@navikt/aksel-icons";
+import {
+  getEditUrl,
+  getMetadataXmlUrl,
+} from "@/app/metadata/[uuid]/_utils/urls";
 import styles from "./ProductActions.module.css";
 
 export async function ProductActions({
-  downloadUrl,
   coverageUrl,
-  metadataXmlUrl,
   uuid,
 }: {
   downloadUrl?: string;
   coverageUrl?: string;
-  metadataXmlUrl?: string;
   uuid: string;
 }) {
-  const EDITOR_BASE_URL = process.env.EDITOR_BASE_URL;
-  const editUrl = `${EDITOR_BASE_URL}/Metadata/Edit?uuid=${uuid}`;
-
   return (
     <div className={styles.actions}>
       <button
@@ -42,31 +40,31 @@ export async function ProductActions({
           Vis dekningskart
         </button>
       )}
-      {metadataXmlUrl && (
-        <button
-          className="ds-button"
-          type="button"
-          data-variant="secondary"
-          data-color="neutral"
-        >
-          <FileTextIcon aria-hidden />
-          Last ned metadata XML
-        </button>
-      )}
+      {/*TODO: er det noen gang denne ikke skal vises?*/}
+      <a
+        data-variant="secondary"
+        target="_blank"
+        rel="noreferrer"
+        href={getMetadataXmlUrl(uuid)}
+        className="ds-button"
+        data-color="neutral"
+      >
+        <FileTextIcon aria-hidden />
+        Last ned metadata XML
+      </a>
       {/*TODO: GN-227 håndtere at noen datasett ikke burde redigeres fra denne editUrl-en*/}
-      {editUrl && (
-        <a
-          data-variant="secondary"
-          target="_blank"
-          rel="noreferrer"
-          href={editUrl}
-          className="ds-button"
-          data-color="neutral"
-        >
-          <PencilIcon aria-hidden />
-          Rediger metadata
-        </a>
-      )}
+      {/*TODO: skal denne noen gang skjules eller vil den alltid dukke opp?*/}
+      <a
+        data-variant="secondary"
+        target="_blank"
+        rel="noreferrer"
+        href={getEditUrl(uuid)}
+        className="ds-button"
+        data-color="neutral"
+      >
+        <PencilIcon aria-hidden />
+        Rediger metadata
+      </a>
     </div>
   );
 }
