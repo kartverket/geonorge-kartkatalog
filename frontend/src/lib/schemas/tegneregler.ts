ad0f8e76f3cfed9030e01cf2f31a7c7d7e74de1a
ingebjrgb <ingebjrgb@gmail.com>
2026-08-11 08:53:36 +0200

    forminsker tegnereglerobjektene som sendes mellom frontend og backend

--- frontend/src/lib/schemas/tegneregler.ts
+++ frontend/src/lib/schemas/tegneregler.ts
@@ -3,22 +3,10 @@ import { z } from "zod";
 export const TegnereglerItemSchema = z.object({
   id: z.string().nullable(),
   label: z.string().nullable(),
-  lang: z.string().nullable(),
-  itemClass: z.string().nullable(),
-  uuid: z.string().nullable(),
   status: z.string().nullable(),
-  description: z.string().nullable(),
-  seoname: z.string().nullable(),
   owner: z.string().nullable(),
-  versionNumber: z.number().int().nullable(),
-  lastUpdated: z.string().nullable(),
   dateSubmitted: z.string().nullable(),
-  dateAccepted: z.string().nullable(),
-  cartographyFile: z.string().nullable(),
-  draftDate: z.string().nullable(),
-  thumbnail: z.string().nullable(),
   documentreference: z.string().nullable(),
-  fairStatusPerCent: z.number().nullable(),
 });
 
 export type TegnereglerItem = z.infer<typeof TegnereglerItemSchema>;
