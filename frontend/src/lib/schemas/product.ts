6da8e6f5fec885869fd9d7460f6f795f04102673
annaaurora <anna-aurora@live.no>
2026-08-17 13:31:00 +0200

    hent felt i frontend

--- frontend/src/lib/schemas/product.ts
+++ frontend/src/lib/schemas/product.ts
@@ -149,6 +149,9 @@ export const LinkedDistributionsSchema = z.object({
   downloadServices: z.array(LinkedDistributionSchema),
   seriesMembers: z.array(LinkedDistributionSchema),
   parentSeries: z.array(LinkedDistributionSchema),
+  relatedDatasets: z.array(LinkedDistributionSchema),
+  serviceLayers: z.array(LinkedDistributionSchema),
+  parentService: z.array(LinkedDistributionSchema),
 });
 
 export type LinkedDistribution = z.infer<typeof LinkedDistributionSchema>;
