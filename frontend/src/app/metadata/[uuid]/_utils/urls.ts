fd9135d7b3cae19a0aab5cf06ecc8e6051b6f1ad
ingebjrgb <ingebjrgb@gmail.com>
2026-08-18 15:27:59 +0200

    fjerner endringer

--- frontend/src/app/metadata/[uuid]/_utils/urls.ts
+++ frontend/src/app/metadata/[uuid]/_utils/urls.ts
@@ -1,4 +1,3 @@
-
 const EDITOR_BASE_URL = process.env.EDITOR_BASE_URL;
 const GEONETWORK_BASE_URL = process.env.GEONETWORK_BASE_URL;
 export const getEditUrl = (id: string) =>
@@ -6,4 +5,3 @@ export const getEditUrl = (id: string) =>
 
 export const getMetadataXmlUrl = (id: string) =>
   `${GEONETWORK_BASE_URL}/srv/nor/csw?service=CSW&request=GetRecordById&version=2.0.2&outputSchema=http://www.isotc211.org/2005/gmd&elementSetName=full&id=${id}`;
-
