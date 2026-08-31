c2365f18648c7bb93ff58731521cd1d9b6c91acb
jonandresen <jon.eirik.lisle.andresen@kartverket.no>
2026-08-31 12:41:18 +0200

    add url to org site

--- frontend/src/app/metadata/[uuid]/_components/ProductHeader.tsx
+++ frontend/src/app/metadata/[uuid]/_components/ProductHeader.tsx
@@ -29,7 +29,7 @@ export function ProductHeader({
       <h1 className={styles.title}>{title ?? "-"}</h1>
       <p className={styles.organization}>
         {getProductTypeString(hierarchyLevel)} fra{" "}
-        <Link href="#">{organization ?? "-"}</Link>
+        <Link href={`https://kartkatalog.geonorge.no/?organizations=${organization}`}>{organization ?? "-"}</Link>
       </p>
     </div>
   );
