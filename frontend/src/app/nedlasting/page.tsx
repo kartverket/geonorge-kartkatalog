9bc9d9800c01762811bec79a2bba77a0b8281fa7
ingebjrgb <ingebjrgb@gmail.com>
2026-09-24 10:54:57 +0200

    legger tul force-dynamic

--- frontend/src/app/nedlasting/page.tsx
+++ frontend/src/app/nedlasting/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { getDownloadInsightGroups } from "@/app/api";
 import { DownloadPageContent } from "./DownloadPageContent";
